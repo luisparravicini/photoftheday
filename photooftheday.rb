efd63a0e3437610fe83010758b1334a8863ebc73
Luis Parravicini <lparravi@gmail.com>
2011-12-12 14:33:21 -0300

    photo urls are stored in a .txt file. Can restore downloading from last url fetched

---
+++ photooftheday.rb
@@ -0,0 +1,32 @@
+require 'mechanize'
+require 'uri'
+
+first_url = 'http://photography.nationalgeographic.com/photography/photo-of-the-day/'
+urls_path = 'photos_urls.txt'
+last_url_path = 'last_url.txt'
+
+agent = Mechanize.new
+agent.user_agent_alias = 'Linux Firefox'
+
+current_url = if File.exists?(last_url_path)
+  File.open(last_url_path, 'r') { |io| io.readline.chomp }
+else
+  first_url
+end
+
+File.open(urls_path, 'a') do |io|
+  while !current_url.nil?
+    doc = agent.get(current_url)
+    puts doc.at('p.publication_time').inner_text
+
+    download_link = doc.at('div.download_link a')
+    io.puts URI.join(current_url, download_link['href']) unless download_link.nil?
+
+    File.open(last_url_path, 'w') { |io| io.puts current_url }
+
+    prev_link = doc.at('div.nav p.prev a')
+    current_url = unless prev_link.nil?
+      URI.join(current_url, prev_link['href']).to_s
+    end
+  end
+end
